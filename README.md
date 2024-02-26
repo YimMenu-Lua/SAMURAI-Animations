7e6f8397291bbcbeeae7a846de7f6d8360355883
SAMURAI <66764345+xesdoog@users.noreply.github.com>
2024-02-26 12:44:10 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# SAMURAI-Animations
-Lua script to play GTA Online Animations using YimMenu.
+## Lua script for Yimmenu to play various GTA 5 animations.
+
+![samurais_animations](https://github.com/xesdoog/SAMURAI-s-Animations/assets/66764345/be26aff9-5ad8-4918-92d4-a9bee03bf3df)
+
+## Usage :
+
+- Download both _samurais_animations.lua_ and _animdata.lua_ and place them in _%AppData%\YimMenu\scripts_
+- Open **YimMenu**, select **SAMURAIs Animations**, choose an animation from the list then press **Play**. You can now close the menu.
+- To stop playing an animation you can either open the menu again and press **Stop** or simply press ' **X** ' on keyboard / ' **LT** ' on controller.
+
+## FAQ :
+
+_Yimmenu already has a built-in animation player. Why use a separate script?_
+
+- This script spawns animation props. It's the only reason why I wrote it.
+- You don't have to configure the animations. The appropriate flags are already setup.
+
+_Why are there only a few animations?_
+
+- This is a work in progress I added a handful of animations for testing purposes. More will be added as I find the time to do so.
+
+## Known Issues :
+
+- ~~Sometimes when stopping an animation, the player's character may clip through the ground. This happens with low positioned animations like sitting or lying down. I don't know why but for now, just tap W after stopping the animation and you should be positioned correctly.~~ **FIXED.**
+
+- ~~When changing to a different animation without stopping the previous one, the prop will become stuck to your character so make sure yo press the **Stop** button before choosing a different animation.~~ **FIXED.** *(n00b mistake)*.
+
+## Credits and Shoutouts :
+
+🚗 #flush_family 🚗
+
+_Lyra_moon_ have fun pissing on the cars you don't like 🤡
+
+[Yimmenu](https://github.com/YimMenu/YimMenu/) For being an awesome project.
---
+++ samurais_animations.lua
@@ -0,0 +1,203 @@
+---@diagnostic disable: undefined-global, lowercase-global
+
+anim_player = gui.get_tab("SAMURAI's Animations")
+
+local animlist = require ("animdata")
+
+local anim_index = 1
+
+anim_player:add_text("Search animations :")
+
+local searchQuery = ""
+
+local is_typing = false
+script.register_looped("Animations", function()
+	if is_typing then
+		PAD.DISABLE_ALL_CONTROL_ACTIONS(0)
+	end
+end)
+
+anim_player:add_imgui(function()
+    searchQuery, used = ImGui.InputText("", searchQuery, 32)
+    if ImGui.IsItemActive() then
+		is_typing = true
+	else
+		is_typing = false
+	end
+    ImGui.PushItemWidth(350)
+end)
+
+local filteredAnims = {}
+local function updatefilteredAnims()
+    filteredAnims = {}
+    for _, anim in ipairs(animlist) do
+        if string.find(string.lower(anim.name), string.lower(searchQuery)) then
+            table.insert(filteredAnims, anim)
+        end
+    end
+end
+
+local function displayFilteredList()
+    updatefilteredAnims()
+    local animNames = {}
+    for _, anim in ipairs(filteredAnims) do
+        table.insert(animNames, anim.name)
+    end
+    anim_index, used = ImGui.ListBox(" ", anim_index, animNames, #filteredAnims)
+end
+
+anim_player:add_imgui(displayFilteredList)
+
+anim_player:add_separator()
+
+anim_player:add_text("TIP : You can stop a currently playing animation \nby pressing 'X' on keyboard or 'LT' on controller.")
+
+anim_player:add_separator()
+
+anim_player:add_imgui(function()
+
+local info = filteredAnims[anim_index+1] or nil
+local ped = PLAYER.GET_PLAYER_PED_SCRIPT_INDEX(PLAYER.PLAYER_ID())
+local coords = ENTITY.GET_ENTITY_COORDS(ped, false)
+local heading = ENTITY.GET_ENTITY_HEADING(ped)
+local forwardX = ENTITY.GET_ENTITY_FORWARD_X(ped)
+local forwardY = ENTITY.GET_ENTITY_FORWARD_Y(ped)
+    if ImGui.Button("Play") then
+        if info ~= nil then
+            local boneIndex = PED.GET_PED_BONE_INDEX(ped, info.boneID)
+            script.run_in_fiber(function(script)
+                STREAMING.REQUEST_ANIM_DICT(info.dict)
+                STREAMING.REQUEST_ANIM_SET(info.anim)
+                coroutine.yield()
+
+                if info.type == 1 then
+                    ENTITY.DELETE_ENTITY(prop)
+                    TASK.CLEAR_PED_TASKS_IMMEDIATELY(ped)
+                    STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
+                    GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
+                    script:sleep(50)
+                    while not STREAMING.HAS_MODEL_LOADED(info.propHash) do
+                        STREAMING.REQUEST_MODEL(info.propHash)
+                        coroutine.yield()
+                    end
+                    STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(info.propHash)
+                    prop = OBJECT.CREATE_OBJECT(info.propHash, 0.0,0.0,0, true, true, false)
+                    ENTITY.ATTACH_ENTITY_TO_ENTITY(prop, ped, boneIndex, info.posx, info.posy, info.posz, info.rotx, info.roty, info.rotz, false, false, false, false, 2, true, 1)
+                    TASK.TASK_PLAY_ANIM(ped, info.dict, info.anim, 4.0, -4.0, -1, info.flag, 1.0, false, false, false)
+                    is_playing_anim = true
+
+                elseif info.type == 2 then
+                    ENTITY.DELETE_ENTITY(prop)
+                    TASK.CLEAR_PED_TASKS_IMMEDIATELY(ped)
+                    STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
+                    GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
+                    script:sleep(50)
+                    while not STREAMING.HAS_NAMED_PTFX_ASSET_LOADED(info.ptfxdict) do
+                        STREAMING.REQUEST_NAMED_PTFX_ASSET(info.ptfxdict)
+                        coroutine.yield()
+                    end
+                    TASK.TASK_PLAY_ANIM(ped, info.dict, info.anim, 4.0, -4.0, -1, info.flag, 0, false, false, false)
+                    script:sleep(400)
+                    GRAPHICS.USE_PARTICLE_FX_ASSET(info.ptfxdict)
+                    loopedFX = GRAPHICS.START_NETWORKED_PARTICLE_FX_LOOPED_ON_ENTITY_BONE(info.ptfxname, ped, info.ptfxOffx, info.ptfxOffy, info.ptfxOffz, 0.0, 0.0, 0.0, boneIndex, info.ptfxscale, false, false, false, 0, 0, 0, 0)
+                    is_playing_anim = true
+
+                elseif info.type == 3 then
+                    ENTITY.DELETE_ENTITY(prop)
+                    TASK.CLEAR_PED_TASKS_IMMEDIATELY(ped)
+                    STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
+                    GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
+                    script:sleep(50)
+                    while not STREAMING.HAS_MODEL_LOADED(info.propHash) do
+                        STREAMING.REQUEST_MODEL(info.propHash)
+                        coroutine.yield()
+                    end
+                    STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(info.propHash)
+                    prop = OBJECT.CREATE_OBJECT(info.propHash, coords.x + (forwardX), coords.y + (forwardY), coords.z, true, true, false)
+                    ENTITY.SET_ENTITY_HEADING(prop, heading -180)
+                    OBJECT.PLACE_OBJECT_ON_GROUND_PROPERLY(prop)
+		    ENTITY.FREEZE_ENTITY_POSITION(prop, true)
+                    TASK.TASK_PLAY_ANIM(ped, info.dict, info.anim, 4.0, -4.0, -1, info.flag, 1.0, false, false, false)
+                    is_playing_anim = true
+
+                elseif info.type == 4 then
+                    ENTITY.DELETE_ENTITY(prop)
+                    TASK.CLEAR_PED_TASKS_IMMEDIATELY(ped)
+                    STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
+                    GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
+                    script:sleep(50)
+                    while not STREAMING.HAS_MODEL_LOADED(info.propHash) do
+                        STREAMING.REQUEST_MODEL(info.propHash)
+                        coroutine.yield()
+                    end
+                    TASK.TASK_PLAY_ANIM(ped, info.dict, info.anim, 4.0, -4.0, -1, info.flag, 1.0, false, false, false)
+                    script:sleep(400)
+                    STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(info.propHash)
+                    prop = OBJECT.CREATE_OBJECT(info.propHash, 0.0,0.0,0, true, true, false)
+                    local bonecoords = PED.GET_PED_BONE_COORDS(ped, info.boneID)
+                    ENTITY.SET_ENTITY_COORDS(prop, bonecoords.x + info.posx, bonecoords.y + info.posy, bonecoords.z + info.posz)
+                    ENTITY.SET_ENTITY_COLLISION(prop, info.propColl, info.propColl)
+                    OBJECT.PLACE_OBJECT_ON_GROUND_PROPERLY(prop)
+                    is_playing_anim = true
+
+                else
+                    ENTITY.DELETE_ENTITY(prop)
+                    TASK.CLEAR_PED_TASKS_IMMEDIATELY(ped)
+                    STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
+                    GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
+                    script:sleep(50)
+                    TASK.TASK_PLAY_ANIM(ped, info.dict, info.anim, 4.0, -4.0, -1, info.flag, 0.0, false, false, false)
+                    is_playing_anim = true
+                end
+            end)
+        end
+    end
+
+ImGui.SameLine()
+
+    if ImGui.Button("Stop") then
+        if info ~= nil and is_playing_anim then
+            script.run_in_fiber(function()
+                ENTITY.DELETE_ENTITY(prop)
+                STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
+                GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
+                TASK.CLEAR_PED_TASKS(ped)
+                TASK.TASK_PLAY_ANIM(ped, "missminuteman_1ig_2", "handsup_base", 8.0, -8.0, -1, 0, 1.0, false, false, false)
+                is_playing_anim = false
+            end)
+        end
+    end
+
+    script.register_looped("Stop Animation", function()
+        if info ~= nil and is_playing_anim then
+            if PAD.IS_CONTROL_PRESSED(0, 252) then
+                ENTITY.DELETE_ENTITY(prop)
+                STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
+                GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
+                TASK.CLEAR_PED_TASKS(ped)
+                TASK.TASK_PLAY_ANIM(ped, "missminuteman_1ig_2", "handsup_base", 8.0, -8.0, -1, 0, 1.0, false, false, false)
+                is_playing_anim = false
+            end
+        end
+    end)
+
+    event.register_handler(menu_event.ScriptsReloaded, function()
+        if info ~= nil and is_playing_anim then
+            ENTITY.DELETE_ENTITY(prop)
+            STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
+            GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
+            TASK.CLEAR_PED_TASKS(ped)
+            is_playing_anim = false
+        end
+    end)
+
+    event.register_handler(menu_event.MenuUnloaded, function()
+        if info ~= nil and is_playing_anim then
+            ENTITY.DELETE_ENTITY(prop)
+            STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
+            GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
+            TASK.CLEAR_PED_TASKS(ped)
+            is_playing_anim = false
+        end
+    end)
+end)

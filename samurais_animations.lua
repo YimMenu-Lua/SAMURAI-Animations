2deba297f7c3c84e1e1b22c3d2a5ec2aa4357a31
SAMURAI <66764345+xesdoog@users.noreply.github.com>
2024-03-01 15:06:21 +0100

    Update samurais_animations.lua
    Fix FPS issue.

--- samurais_animations.lua
+++ samurais_animations.lua
@@ -39,13 +39,13 @@ end
 
 local function displayFilteredList()
     updatefilteredAnims()
-
     local animNames = {}
     for _, anim in ipairs(filteredAnims) do
         table.insert(animNames, anim.name)
     end
     anim_index, used = ImGui.ListBox(" ", anim_index, animNames, #filteredAnims)
 end
+
 anim_player:add_imgui(displayFilteredList)
 
 anim_player:add_separator()
@@ -62,21 +62,35 @@ local coords = ENTITY.GET_ENTITY_COORDS(ped, false)
 local heading = ENTITY.GET_ENTITY_HEADING(ped)
 local forwardX = ENTITY.GET_ENTITY_FORWARD_X(ped)
 local forwardY = ENTITY.GET_ENTITY_FORWARD_Y(ped)
-    if ImGui.Button("Play") then
-        if info ~= nil then
-            local boneIndex = PED.GET_PED_BONE_INDEX(ped, info.boneID)
-            script.run_in_fiber(function(script)
-                STREAMING.REQUEST_ANIM_DICT(info.dict)
-                STREAMING.REQUEST_ANIM_SET(info.anim)
-                coroutine.yield()
+local is_playing_anim = false
+local boneIndex = PED.GET_PED_BONE_INDEX(ped, info.boneID)
+local bonecoords = PED.GET_PED_BONE_COORDS(ped, info.boneID)
+local function cleanup()
+    script.run_in_fiber(function(fixthisshit)
+        TASK.CLEAR_PED_TASKS(ped)
+        ENTITY.DELETE_ENTITY(prop1)
+        ENTITY.SET_ENTITY_AS_NO_LONGER_NEEDED(prop1)
+        ENTITY.DELETE_ENTITY(prop2)
+        ENTITY.SET_ENTITY_AS_NO_LONGER_NEEDED(prop2)
+        GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
+        while STREAMING.DOES_ANIM_DICT_EXIST(info.dict) and STREAMING.DOES_ANIM_SET_EXIST(info.anim) do
+            STREAMING.REMOVE_ANIM_DICT(info.dict)
+            STREAMING.REMOVE_ANIM_SET(info.anim)
+            coroutine.yield()
+        end
+        while STREAMING.HAS_NAMED_PTFX_ASSET_LOADED(info.ptfxdict) do
+            STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
+            coroutine.yield()
+        end
+        fixthisshit:sleep()
+    end)
+end
 
-                if info.type == 1 then
-                    ENTITY.DELETE_ENTITY(prop1)
-                    ENTITY.DELETE_ENTITY(prop2)
-                    TASK.CLEAR_PED_TASKS(ped)
-                    STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
-                    GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
-                    script:sleep(50)
+    if ImGui.Button("Play") then
+        if info then
+            if info.type == 1 then
+                cleanup()
+                script.run_in_fiber(function()
                     while not STREAMING.HAS_MODEL_LOADED(info.prop1) do
                         STREAMING.REQUEST_MODEL(info.prop1)
                         coroutine.yield()
@@ -84,34 +98,41 @@ local forwardY = ENTITY.GET_ENTITY_FORWARD_Y(ped)
                     prop1 = OBJECT.CREATE_OBJECT(info.prop1, 0.0, 0.0, 0, true, true, false)
                     ENTITY.ATTACH_ENTITY_TO_ENTITY(prop1, ped, boneIndex, info.posx, info.posy, info.posz, info.rotx, info.roty, info.rotz, false, false, false, false, 2, true, 1)
                     STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(info.prop1)
+                    while not STREAMING.HAS_ANIM_DICT_LOADED(info.dict) and not STREAMING.HAS_ANIM_SET_LOADED(info.anim) do
+                        STREAMING.REQUEST_ANIM_DICT(info.dict)
+                        STREAMING.REQUEST_ANIM_SET(info.anim)
+                        coroutine.yield()
+                    end
                     TASK.TASK_PLAY_ANIM(ped, info.dict, info.anim, 4.0, -4.0, -1, info.flag, 1.0, false, false, false)
-                    STREAMING.REMOVE_ANIM_DICT(info.dict)
                     is_playing_anim = true
+                end)
 
-                elseif info.type == 2 then
-                    ENTITY.DELETE_ENTITY(prop1)
-                    ENTITY.DELETE_ENTITY(prop2)
-                    TASK.CLEAR_PED_TASKS(ped)
-                    STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
-                    GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
-                    script:sleep(50)
+            elseif info.type == 2 then
+                cleanup()
+                script.run_in_fiber(function(type2)
                     while not STREAMING.HAS_NAMED_PTFX_ASSET_LOADED(info.ptfxdict) do
                         STREAMING.REQUEST_NAMED_PTFX_ASSET(info.ptfxdict)
                         coroutine.yield()
                     end
+                    while not STREAMING.HAS_ANIM_DICT_LOADED(info.dict) and not STREAMING.HAS_ANIM_SET_LOADED(info.anim) do
+                        STREAMING.REQUEST_ANIM_DICT(info.dict)
+                        STREAMING.REQUEST_ANIM_SET(info.anim)
+                        coroutine.yield()
+                    end
                     TASK.TASK_PLAY_ANIM(ped, info.dict, info.anim, 4.0, -4.0, -1, info.flag, 0, false, false, false)
-                    script:sleep(400)
+                    type2:sleep(400)
                     GRAPHICS.USE_PARTICLE_FX_ASSET(info.ptfxdict)
                     loopedFX = GRAPHICS.START_NETWORKED_PARTICLE_FX_LOOPED_ON_ENTITY_BONE(info.ptfxname, ped, info.ptfxOffx, info.ptfxOffy, info.ptfxOffz, 0.0, 0.0, 0.0, boneIndex, info.ptfxscale, false, false, false, 0, 0, 0, 0)
+                    while STREAMING.HAS_NAMED_PTFX_ASSET_LOADED(info.ptfxdict) do
+                        STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
+                        coroutine.yield()
+                    end
                     is_playing_anim = true
+                end)
 
-                elseif info.type == 3 then
-                    ENTITY.DELETE_ENTITY(prop1)
-                    ENTITY.DELETE_ENTITY(prop2)
-                    TASK.CLEAR_PED_TASKS(ped)
-                    STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
-                    GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
-                    script:sleep(50)
+            elseif info.type == 3 then
+                cleanup()
+                script.run_in_fiber(function()
                     while not STREAMING.HAS_MODEL_LOADED(info.prop1) do
                         STREAMING.REQUEST_MODEL(info.prop1)
                         coroutine.yield()
@@ -120,143 +141,110 @@ local forwardY = ENTITY.GET_ENTITY_FORWARD_Y(ped)
                     ENTITY.SET_ENTITY_HEADING(prop1, heading)
                     OBJECT.PLACE_OBJECT_ON_GROUND_PROPERLY(prop1)
                     STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(info.prop1)
+                    while not STREAMING.HAS_ANIM_DICT_LOADED(info.dict) and not STREAMING.HAS_ANIM_SET_LOADED(info.anim) do
+                        STREAMING.REQUEST_ANIM_DICT(info.dict)
+                        STREAMING.REQUEST_ANIM_SET(info.anim)
+                        coroutine.yield()
+                    end
                     TASK.TASK_PLAY_ANIM(ped, info.dict, info.anim, 4.0, -4.0, -1, info.flag, 1.0, false, false, false)
                     is_playing_anim = true
+                end)
 
-                elseif info.type == 4 then
-                    ENTITY.DELETE_ENTITY(prop1)
-                    ENTITY.DELETE_ENTITY(prop2)
-                    TASK.CLEAR_PED_TASKS(ped)
-                    STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
-                    GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
-                    script:sleep(50)
+            elseif info.type == 4 then
+                cleanup()
+                script.run_in_fiber(function(type4)
                     while not STREAMING.HAS_MODEL_LOADED(info.prop1) do
                         STREAMING.REQUEST_MODEL(info.prop1)
                         coroutine.yield()
                     end
+                    while not STREAMING.HAS_ANIM_DICT_LOADED(info.dict) and not STREAMING.HAS_ANIM_SET_LOADED(info.anim) do
+                        STREAMING.REQUEST_ANIM_DICT(info.dict)
+                        STREAMING.REQUEST_ANIM_SET(info.anim)
+                        coroutine.yield()
+                    end
                     TASK.TASK_PLAY_ANIM(ped, info.dict, info.anim, 4.0, -4.0, -1, info.flag, 1.0, false, false, false)
-                    script:sleep(400)
                     prop1 = OBJECT.CREATE_OBJECT(info.prop1, 0.0, 0.0, 0, true, true, false)
-                    local bonecoords = PED.GET_PED_BONE_COORDS(ped, info.boneID)
-                    ENTITY.SET_ENTITY_COORDS(prop1, bonecoords.x + info.posx, bonecoords.y + info.posy, bonecoords.z + info.posz)
-                    ENTITY.SET_ENTITY_COLLISION(prop1, info.propColl, info.propColl)
+                    type4:sleep(200)
+                    ENTITY.SET_ENTITY_COORDS_NO_OFFSET(prop1, bonecoords.x + info.posx, bonecoords.y + info.posy, bonecoords.z + info.posz)
                     OBJECT.PLACE_OBJECT_ON_GROUND_PROPERLY(prop1)
+                    ENTITY.SET_ENTITY_COLLISION(prop1, info.propColl, info.propColl)
                     STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(info.prop1)
                     is_playing_anim = true
-
-                elseif info.type == 5 then
-                    ENTITY.DELETE_ENTITY(prop1)
-                    ENTITY.DELETE_ENTITY(prop2)
-                    TASK.CLEAR_PED_TASKS(ped)
-                    STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
-                    GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
-                    script:sleep(50)
-                    while not STREAMING.HAS_MODEL_LOADED(info.prop1) do
-                        STREAMING.REQUEST_MODEL(info.prop1)
+                end)
+            else
+                cleanup()
+                script.run_in_fiber(function(script)
+                    while not STREAMING.HAS_ANIM_DICT_LOADED(info.dict) and not STREAMING.HAS_ANIM_SET_LOADED(info.anim) do
+                        STREAMING.REQUEST_ANIM_DICT(info.dict)
+                        STREAMING.REQUEST_ANIM_SET(info.anim)
                         coroutine.yield()
                     end
-                    prop1 = OBJECT.CREATE_OBJECT(info.prop1, 0.0, 0.0, 0, true, true, false)
-                    ENTITY.ATTACH_ENTITY_TO_ENTITY(prop1, ped, boneIndex, info.posx, info.posy, info.posz, info.rotx, info.roty, info.rotz, false, false, false, false, 2, true, 1)
-                    STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(info.prop1)
-                    script:sleep(50)
-                    while not STREAMING.HAS_MODEL_LOADED(info.prop2) do
-                        STREAMING.REQUEST_MODEL(info.prop2)
-                        coroutine.yield()
-                    end
-                    prop2 = OBJECT.CREATE_OBJECT(info.prop2, coords.x + (forwardX), coords.y + (forwardY), coords.z, true, true, false)
-                    ENTITY.SET_ENTITY_HEADING(prop2, heading)
-                    OBJECT.PLACE_OBJECT_ON_GROUND_PROPERLY(prop2)
-                    STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(info.prop2)
-                    script:sleep(50)
-                    while not STREAMING.HAS_NAMED_PTFX_ASSET_LOADED(info.ptfxdict) do
-                        STREAMING.REQUEST_NAMED_PTFX_ASSET(info.ptfxdict)
-                        coroutine.yield()
-                    end
-                    GRAPHICS.USE_PARTICLE_FX_ASSET(info.ptfxdict)
-                    loopedFX = GRAPHICS.START_NETWORKED_PARTICLE_FX_LOOPED_ON_ENTITY(info.ptfxname, prop2, info.ptfxOffx, info.ptfxOffy, info.ptfxOffz, 0.0, 0.0, 0.0, info.ptfxscale, false, false, false, 0, 0, 0, 0)
-                    TASK.TASK_PLAY_ANIM(ped, info.dict, info.anim, 4.0, -4.0, -1, info.flag, 1.0, false, false, false)
-                    is_playing_anim = true
-
-                else
-                    ENTITY.DELETE_ENTITY(prop1)
-                    ENTITY.DELETE_ENTITY(prop2)
-                    TASK.CLEAR_PED_TASKS(ped)
-                    STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
-                    GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
-                    script:sleep(50)
                     TASK.TASK_PLAY_ANIM(ped, info.dict, info.anim, 4.0, -4.0, -1, info.flag, 0.0, false, false, false)
                     is_playing_anim = true
-                end
-            end)
+                end)
+            end
         end
     end
 
 ImGui.SameLine()
 
     if ImGui.Button("Stop") then
-        if info ~= nil and is_playing_anim then
-            script.run_in_fiber(function(script)
-                ENTITY.DELETE_ENTITY(prop1)
-                ENTITY.DELETE_ENTITY(prop2)
-                STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
-                GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
-                STREAMING.REMOVE_PTFX_ASSET()
-		        STREAMING.REMOVE_ANIM_DICT(info.dict)
-                TASK.CLEAR_PED_TASKS(ped)
-                -- //fix player clipping through the ground after ending low-positioned anims//
-                local current_coords = ENTITY.GET_ENTITY_COORDS(ped)
-                ENTITY.SET_ENTITY_COORDS_NO_OFFSET(ped, current_coords.x, current_coords.y, 0, true, false, false)
-                script:sleep(3000)
-                if STREAMING.HAS_ANIM_DICT_LOADED(info.dict) then gui.show_warning("FU%CK!!!") end
-                is_playing_anim = false
-            end)
-        end
+        cleanup()
+        -- //fix player clipping through the ground after ending low-positioned anims//
+        local current_coords = ENTITY.GET_ENTITY_COORDS(ped)
+        ENTITY.SET_ENTITY_COORDS_NO_OFFSET(ped, current_coords.x, current_coords.y, current_coords.z, true, false, false)
+        is_playing_anim = false
     end
 
     script.register_looped("Stop Animation", function()
-        if info ~= nil and is_playing_anim then
-            if PAD.IS_CONTROL_PRESSED(0, 252) then
-                ENTITY.DELETE_ENTITY(prop1)
-                ENTITY.DELETE_ENTITY(prop2)
-                STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
-                GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
-		        STREAMING.REMOVE_ANIM_DICT(info.dict)
-                TASK.CLEAR_PED_TASKS(ped)
-                -- //fix player clipping through the ground after ending low-positioned anims//
-                local current_coords = ENTITY.GET_ENTITY_COORDS(ped)
-                ENTITY.SET_ENTITY_COORDS_NO_OFFSET(ped, current_coords.x, current_coords.y, current_coords.z, true, false, false)
-                is_playing_anim = false
-            end
+        if is_playing_anim and PAD.IS_CONTROL_PRESSED(0, 252) then
+            cleanup()
+            -- //fix player clipping through the ground after ending low-positioned anims//
+            local current_coords = ENTITY.GET_ENTITY_COORDS(ped)
+            ENTITY.SET_ENTITY_COORDS_NO_OFFSET(ped, current_coords.x, current_coords.y, current_coords.z, true, false, false)
+            is_playing_anim = false
         end
     end)
 
     event.register_handler(menu_event.ScriptsReloaded, function()
-        if info ~= nil and is_playing_anim then
-            ENTITY.DELETE_ENTITY(prop1)
-            ENTITY.DELETE_ENTITY(prop2)
-            STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
             GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
-	        STREAMING.REMOVE_ANIM_DICT(info.dict)
+            ENTITY.DELETE_ENTITY(prop1)
+            ENTITY.SET_ENTITY_AS_NO_LONGER_NEEDED(prop1)
+            ENTITY.DELETE_ENTITY(prop2)
+            ENTITY.SET_ENTITY_AS_NO_LONGER_NEEDED(prop2)
             TASK.CLEAR_PED_TASKS_IMMEDIATELY(ped)
+            while STREAMING.HAS_NAMED_PTFX_ASSET_LOADED(info.ptfxdict) do
+                STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
+                coroutine.yield()
+            end
+            while STREAMING.DOES_ANIM_DICT_EXIST(info.dict) and STREAMING.DOES_ANIM_SET_EXIST(info.anim) do
+                STREAMING.REMOVE_ANIM_DICT(info.dict)
+                STREAMING.REMOVE_ANIM_SET(info.anim)
+                coroutine.yield()
+            end
             -- //fix player clipping through the ground after ending low-positioned anims//
             local current_coords = ENTITY.GET_ENTITY_COORDS(ped)
             ENTITY.SET_ENTITY_COORDS_NO_OFFSET(ped, current_coords.x, current_coords.y, current_coords.z, true, false, false)
-            is_playing_anim = false
-        end
     end)
 
     event.register_handler(menu_event.MenuUnloaded, function()
-        if info ~= nil and is_playing_anim then
-            ENTITY.DELETE_ENTITY(prop1)
-            ENTITY.DELETE_ENTITY(prop2)
-            STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
             GRAPHICS.STOP_PARTICLE_FX_LOOPED(loopedFX)
-	        STREAMING.REMOVE_ANIM_DICT(info.dict)
+            ENTITY.DELETE_ENTITY(prop1)
+            ENTITY.SET_ENTITY_AS_NO_LONGER_NEEDED(prop1)
+            ENTITY.DELETE_ENTITY(prop2)
+            ENTITY.SET_ENTITY_AS_NO_LONGER_NEEDED(prop2)
             TASK.CLEAR_PED_TASKS_IMMEDIATELY(ped)
+            while STREAMING.HAS_NAMED_PTFX_ASSET_LOADED(info.ptfxdict) do
+                STREAMING.REMOVE_NAMED_PTFX_ASSET(info.ptfxdict)
+                coroutine.yield()
+            end
+            while STREAMING.DOES_ANIM_DICT_EXIST(info.dict) and STREAMING.DOES_ANIM_SET_EXIST(info.anim) do
+                STREAMING.REMOVE_ANIM_DICT(info.dict)
+                STREAMING.REMOVE_ANIM_SET(info.anim)
+                coroutine.yield()
+            end
             -- //fix player clipping through the ground after ending low-positioned anims//
             local current_coords = ENTITY.GET_ENTITY_COORDS(ped)
             ENTITY.SET_ENTITY_COORDS_NO_OFFSET(ped, current_coords.x, current_coords.y, current_coords.z, true, false, false)
-            is_playing_anim = false
-        end
     end)
 end)
